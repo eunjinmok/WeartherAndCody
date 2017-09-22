4eba0fb0fea7153f86e0e70dc9a39692c72e0a71
jin <jinx94@naver.com>
2017-09-22 15:56:02 +0900

    날씨랑 날씨 이미지랑 가져옴

--- app/src/main/java/com/eunjin/weatherandcody/LocationActivity.java
+++ app/src/main/java/com/eunjin/weatherandcody/LocationActivity.java
@@ -1,13 +1,29 @@
 package com.eunjin.weatherandcody;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LocationActivity extends AppCompatActivity {
+
+    @BindView(R.id.cityname_edit_view)
+    EditText citynameEditView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+        ButterKnife.bind(this);
+    }
+
+    public void onClickedEnterButton(View view) {
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        intent.putExtra("cityName",citynameEditView.getText().toString());
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/eunjin/weatherandcody/MainActivity.java
+++ app/src/main/java/com/eunjin/weatherandcody/MainActivity.java
@@ -1,9 +1,18 @@
 package com.eunjin.weatherandcody;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.eunjin.weatherandcody.model.current.CurrentWeather;
+import com.eunjin.weatherandcody.retrofit.WeatherUtil;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView mUvTextView;
     private TextView mDustTextView;
     private ImageView mCodyImageView;
+    private WeatherUtil mWeatherUtil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +36,34 @@ public class MainActivity extends AppCompatActivity {
         mUvTextView = (TextView) findViewById(R.id.uv_text_view);
         mDustTextView = (TextView) findViewById(R.id.dust_text_view);
         mCodyImageView = (ImageView) findViewById(R.id.cody_image_view);
+        mWeatherUtil = new WeatherUtil();
+
+        Intent intent = getIntent();
+        String intentCityName = intent.getStringExtra("cityName");
+
+        search(intentCityName);
+
+    }
+
+    private void search(String cityName) {
+        mWeatherUtil.getApiService().getCurrentWeather(cityName).enqueue(new Callback<CurrentWeather>() {
+            @Override
+            public void onResponse(Call<CurrentWeather> call, Response<CurrentWeather> response) {
+
+                String url = "http://openweathermap.org/img/w/" + response.body().getWeather().get(0).getIcon() + ".png";
+                Glide.with(MainActivity.this).load(url).into(mWeatherImageView);
+                mWeatherTextView.setText("날씨 : " + response.body().getWeather().get(0).getDescription());
+                mTempTextView.setText("기온 : " + response.body().getMain().getTemp());
+                mUvTextView.setText("자외선 : ");
+                mDustTextView.setText("미세먼지 : ");
+
+
+            }
+
+            @Override
+            public void onFailure(Call<CurrentWeather> call, Throwable t) {
+
+            }
+        });
     }
 }
